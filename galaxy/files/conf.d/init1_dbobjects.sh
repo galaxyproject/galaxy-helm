ef09517360ff611c154774301ad2c0ea2a9c729d
Pablo Moreno <pcm32@users.noreply.github.com>
2020-03-18 21:54:52 +0000

    Creates database and user if they don't exist only

--- galaxy/files/conf.d/init1_dbobjects.sh
+++ galaxy/files/conf.d/init1_dbobjects.sh
@@ -3,8 +3,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 -v GXYPASSWORD="'$GALAXY_DB_USER_PASSWORD'" --username "$POSTGRESQL_USERNAME" --dbname "$POSTGRESQL_DATABASE" <<-EOSQL
-		CREATE DATABASE galaxy;
-		CREATE USER {{.Values.postgresql.galaxyDatabaseUser}};
+		CREATE IF NOT EXISTS DATABASE galaxy;
+		CREATE IF NOT EXISTS USER {{.Values.postgresql.galaxyDatabaseUser}};
 		ALTER ROLE {{.Values.postgresql.galaxyDatabaseUser}} WITH PASSWORD :GXYPASSWORD;
 		GRANT ALL PRIVILEGES ON DATABASE galaxy TO {{.Values.postgresql.galaxyDatabaseUser}};
 		ALTER DATABASE galaxy OWNER TO {{.Values.postgresql.galaxyDatabaseUser}};

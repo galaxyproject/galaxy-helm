dfbf64b6488befce660479dd2032d413a26087ca
luke <luke.c.sargent@gmail.com>
2020-04-01 22:55:09 +0000

    Job probes (#127)
    * add probe script
    
    * add fail condition: no handler found
    
    * removing offending underscore
    
    * adding logic for startup and liveness probes
    
    * missed a comma :(
    
    * replacing startupProbe with readinessProbe because startupProbe is only in k8s versions >1.16
    
    * misunderstood difference between readiness and liveness probes
    
    * changing threshold and delay values for probes to be more robust
    
    * variablizing stuff from deployment yaml into values file
    
    * whoops didn't save a file
    
    * templating didn't work as planned
    
    * templating didn't work as planned part 2
    
    * adding liveness probe variables to values yaml
    
    * ALWAYS SAVE YOUR FILES BEFORE COMMITTING
    
    * variablize timeout interval for db probe script
    
    * mirroring values-cvmfs changes into values
    
    * testing refactoring how probe is provisioned (out of values into configmap)
    
    * still figuring out this templating
    
    * configMap revisions to be syntactically correct
    
    * indentation fun with YAML->JSON
    
    * refining python script
    
    * revisions suggested by flake8 linter
    
    * cleaning comments
    
    * removing an option that the user probably shouldn't be exposed to
    
    * add probe script
    
    * add fail condition: no handler found
    
    * removing offending underscore
    
    * adding logic for startup and liveness probes
    
    * missed a comma :(
    
    * replacing startupProbe with readinessProbe because startupProbe is only in k8s versions >1.16
    
    * misunderstood difference between readiness and liveness probes
    
    * changing threshold and delay values for probes to be more robust
    
    * variablizing stuff from deployment yaml into values file
    
    * whoops didn't save a file
    
    * templating didn't work as planned
    
    * templating didn't work as planned part 2
    
    * adding liveness probe variables to values yaml
    
    * ALWAYS SAVE YOUR FILES BEFORE COMMITTING
    
    * variablize timeout interval for db probe script
    
    * mirroring values-cvmfs changes into values
    
    * Fixing job handlers scaling
    
    * testing refactoring how probe is provisioned (out of values into configmap)
    
    * still figuring out this templating
    
    * configMap revisions to be syntactically correct
    
    * indentation fun with YAML->JSON
    
    * refining python script
    
    * revisions suggested by flake8 linter
    
    * cleaning comments
    
    * removing an option that the user probably shouldn't be exposed to
    
    Co-authored-by: almahmoud <amahmou4@jhu.edu>

---
+++ galaxy/scripts/probedb.py
@@ -0,0 +1,91 @@
+import argparse
+import logging
+import psycopg2
+import socket
+import sys
+
+
+from datetime import datetime
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+log.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def check_rows(connection_string, query_string, interval=60):
+    error_prefix = "DB PROBE ERROR"
+
+    def less_than_interval(a, b, interval=60):
+        if (b-a).seconds > interval:
+            log.debug("time delta {} > {}s".format((b-a).seconds, interval))
+            return 1
+        log.debug("time delta {} <= {}s".format((b-a).seconds, interval))
+        return 0
+
+    conn = psycopg2.connect(connection_string)
+    cur = conn.cursor()
+    cur.execute(query_string)
+    rows = cur.fetchall()
+    if len(rows) != 1:
+        log.debug("{}: there should be only one row, but {} found".format(
+            error_prefix, len(rows)))
+        return 1
+    for row in rows:
+        now = datetime.now()
+        return less_than_interval(row[0], now, interval)
+    log.info("{}: failure to determine interval".format(error_prefix))
+    return 1
+
+
+def main():
+    # allowable seconds since db timestamp
+    default_interval = 60
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--interval",
+                        type=int,
+                        help="how long (in seconds) to wait between probes")
+    parser.add_argument("-v", "--verbose",
+                        help="enable verbose output", action="store_true")
+    parser.add_argument("-e", "--environmentvariable",
+                        help="environment variable with connection string")
+    parser.add_argument("-u", "--username", help="postgres db user name")
+    parser.add_argument("-p", "--password", help="postgres db user pass")
+    parser.add_argument("-d", "--dbname",
+                        help="postgres db name to connect to")
+    parser.add_argument("-b", "--dbhost",
+                        help="name of host where postgres lives")
+    parser.add_argument("-o", "--hostname",
+                        help="handler host name to be checked in postgres db",
+                        default=socket.gethostname())
+
+    parsed_args = parser.parse_args()
+
+    if parsed_args.verbose:
+        log.setLevel(logging.DEBUG)
+
+    connection_string = ""
+
+    if parsed_args.environmentvariable:
+        connection_string = parsed_args.environmentvariable
+    else:
+        connection_string = "dbname='" + parsed_args.dbname +\
+                            "' user='" + parsed_args.username +\
+                            "' host='" + parsed_args.dbhost +\
+                            "' password='" + parsed_args.password + "'"
+
+    query_string = "SELECT update_time FROM worker_process WHERE hostname='" +\
+        parsed_args.hostname + "';"
+
+    log.debug("provided arguments: \n" + str(parsed_args))
+    log.debug("connection string: {}".format(connection_string))
+    log.debug("query string: {}".format(query_string))
+
+    return check_rows(connection_string, query_string,
+                      parsed_args.interval if parsed_args.interval
+                      else default_interval)
+
+
+if __name__ == '__main__':
+    sys.exit(main())

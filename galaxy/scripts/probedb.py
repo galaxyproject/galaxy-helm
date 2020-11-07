0f6d9329b6114656183da2c1eae5abf3171be378
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-11-08 00:19:20 +0530

    Make readiness probe query by both server name and hostname, and some refactoring (#179)

--- galaxy/scripts/probedb.py
+++ galaxy/scripts/probedb.py
@@ -13,7 +13,6 @@ log.addHandler(logging.StreamHandler(sys.stdout))
 
 
 def check_rows(connection_string, query_string, interval=60):
-    error_prefix = "DB PROBE ERROR"
 
     def less_than_interval(a, b, interval=60):
         if (b-a).seconds > interval:
@@ -26,15 +25,10 @@ def check_rows(connection_string, query_string, interval=60):
     cur = conn.cursor()
     cur.execute(query_string)
     rows = cur.fetchall()
-    if len(rows) != 1:
-        log.debug("{}: there should be only one row, but {} found".format(
-            error_prefix, len(rows)))
-        return 1
-    for row in rows:
-        now = datetime.now()
-        return less_than_interval(row[0], now, interval)
-    log.info("{}: failure to determine interval".format(error_prefix))
-    return 1
+    log.debug(f"matching rows {str(rows)}")
+    # At least one matching handler must be alive
+    matches = any(less_than_interval(row[0], datetime.now(), interval) for row in rows)
+    return 0 if matches else 1
 
 
 def main():
@@ -43,48 +37,32 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i", "--interval",
-                        type=int,
-                        help="how long (in seconds) to wait between probes")
-    parser.add_argument("-v", "--verbose",
-                        help="enable verbose output", action="store_true")
-    parser.add_argument("-e", "--environmentvariable",
-                        help="environment variable with connection string")
-    parser.add_argument("-u", "--username", help="postgres db user name")
-    parser.add_argument("-p", "--password", help="postgres db user pass")
-    parser.add_argument("-d", "--dbname",
-                        help="postgres db name to connect to")
-    parser.add_argument("-b", "--dbhost",
-                        help="name of host where postgres lives")
+    parser.add_argument("-c", "--connection", required=True,
+                        help="connection string to galaxy database")
+    parser.add_argument("-s", "--server_name", required=True,
+                        help="handler's server_name to be checked in postgres db")
     parser.add_argument("-o", "--hostname",
                         help="handler host name to be checked in postgres db",
                         default=socket.gethostname())
+    parser.add_argument("-i", "--interval",
+                        type=int,
+                        help="how long (in seconds) since heartbeat to determine probe failure")
+    parser.add_argument("-v", "--verbose",
+                        help="enable verbose output", action="store_true")
 
-    parsed_args = parser.parse_args()
+    args = parser.parse_args()
 
-    if parsed_args.verbose:
+    if args.verbose:
         log.setLevel(logging.DEBUG)
 
-    connection_string = ""
+    db_query = f"SELECT update_time FROM worker_process WHERE hostname='{args.hostname}'" \
+               f" AND server_name='{args.server_name}';"
 
-    if parsed_args.environmentvariable:
-        connection_string = parsed_args.environmentvariable
-    else:
-        connection_string = "dbname='" + parsed_args.dbname +\
-                            "' user='" + parsed_args.username +\
-                            "' host='" + parsed_args.dbhost +\
-                            "' password='" + parsed_args.password + "'"
+    log.debug(f"provided arguments: {str(args)}")
+    log.debug(f"db connection string: {args.connection}")
+    log.debug(f"db query: {db_query}")
 
-    query_string = "SELECT update_time FROM worker_process WHERE hostname='" +\
-        parsed_args.hostname + "';"
-
-    log.debug("provided arguments: \n" + str(parsed_args))
-    log.debug("connection string: {}".format(connection_string))
-    log.debug("query string: {}".format(query_string))
-
-    return check_rows(connection_string, query_string,
-                      parsed_args.interval if parsed_args.interval
-                      else default_interval)
+    return check_rows(args.connection, db_query, args.interval if args.interval else default_interval)
 
 
 if __name__ == '__main__':
